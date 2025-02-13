c62136dd25fc4daac0292c8bc788641229f7b42d
vasuvamsi07 <130281518+vasuvamsi07@users.noreply.github.com>
2025-02-13 23:39:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Load the trained model and vectorizer
+with open("model.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
+
+with open("vectorizer.pkl", "rb") as vectorizer_file:
+    vectorizer = pickle.load(vectorizer_file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['message']
+    transformed_data = vectorizer.transform([data])
+    prediction = model.predict(transformed_data)[0]
+    
+    return jsonify({"spam": bool(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
